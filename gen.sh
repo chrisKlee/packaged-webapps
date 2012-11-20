37804e2f7bec797abdee26e8c229fe2c6be19672
Marcos Caceres <marcosscaceres@gmail.com>
2012-11-20 12:09:52 +0000

    fixed up gen script

--- gen.sh
+++ gen.sh
@@ -11,6 +11,7 @@ if [ $? -ne 0 ]; then
        exit 1
 fi
 anolis --pubdate="$1" --output-encoding=utf8 --omit-optional-tags --quote-attr-values --w3c-compat --w3c-shortname="widgets" --filter=".dontpublish, .now3c" --w3c-status=REC Overview.src.html $TMPFILE
+
 git checkout gh-pages
 cp $TMPFILE index.html
 
@@ -20,8 +21,10 @@ if command -v tidy >/dev/null 2>&1; then
 fi
 
 git commit -m "Regenerated file"
+git push
 
 #dependency: sudo gem install terminal-notifier
 if command -v terminal-notifier >/dev/null 2>&1; then
     terminal-notifier -message "Finished processing ${PWD##*/}" -title "Anolis Processing done" -execute "open $PWD/index.html" -group "Anolis"
 fi
+git checkout master
