600b3f806822f8fd991f8ec839403476245ab858
Marcos Caceres <marcosscaceres@gmail.com>
2012-11-20 11:56:06 +0000

    Added tidy support and notification support

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$1" ]
 then
-        echo "Need a pub date, like 22 Nov 2012"
+        echo "Need a pub date, like '22 Nov 2012'"
        	exit 1
 fi
 tempfoo=`basename $0`
@@ -13,4 +13,15 @@ fi
 anolis --pubdate="$1" --output-encoding=utf8 --omit-optional-tags --quote-attr-values --w3c-compat --w3c-shortname="widgets" --filter=".dontpublish, .now3c" --w3c-status=REC Overview.src.html $TMPFILE
 git checkout gh-pages
 cp $TMPFILE index.html
+
+#dependency: html5 tidy https://github.com/w3c/tidy-html5
+if command -v tidy >/dev/null 2>&1; then
+	tidy -q -utf8 -i -o index.html index.html
+fi
+
 git commit -m "Regenerated file"
+
+#dependency: sudo gem install terminal-notifier
+if command -v terminal-notifier >/dev/null 2>&1; then
+    terminal-notifier -message "Finished processing ${PWD##*/}" -title "Anolis Processing done" -execute "open $PWD/index.html" -group "Anolis"
+fi
